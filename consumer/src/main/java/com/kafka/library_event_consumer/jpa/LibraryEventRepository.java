a83d914a137fa62305e8bd8c3808832a9d546e54
Hassan Kamel <hkamel@deloitte.com>
2024-08-27 12:36:01 +0300

    delete

--- consumer/src/main/java/com/kafka/library_event_consumer/jpa/LibraryEventRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kafka.library_event_consumer.jpa;
-
-import com.kafka.library_event_consumer.entity.LibraryEvent;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibraryEventRepository extends CrudRepository<LibraryEvent, Integer> {
-}
--- producer/src/main/java/com/kafka/library_events_producer/dto/LibraryEventType.java
+++
@@ -1,6 +0,0 @@
-package com.kafka.library_events_producer.dto;
-
-public enum LibraryEventType {
-    NEW,
-    UPDATE
-}
